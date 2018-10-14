ce32d78c00fe73a19e423bcc4cfe07d6f6012964
David Sanderson <dsanderson3@cnm.edu>
2018-10-14 09:06:44 -0600

    added proper directory structure

---
+++ documentation/milestone-1.php
@@ -0,0 +1,19 @@
+<<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>PWP Milestone 1 Doc</title>
+	</head>
+	<body>
+		<h2>Purpose:</h2>
+		<p></p>
+		<h2>Audience:</h2>
+		<p></p>
+		<h2>Goal:</h2>
+		<p></p>
+
+	</body>
+</html>
