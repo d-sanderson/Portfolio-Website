a4ad47bcca1f272286712a5bc032e9fdf972631a
David Sanderson <dsanderson3@cnm.edu>
2018-10-17 22:13:11 -0600

    added proper directory structure

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -13,18 +13,20 @@
 
 		<h2>Purpose:</h2>
 
-		<p>The purpose of this project is to create a digital portfolio where I can exhibit my technical expertise and experience with web-based technologies.</p>
+		<p>The purpose of this project is to create a digital portfolio where I can exhibit my technical expertise and
+			experience with web-based technologies.</p>
 
 		<h2>Audience:</h2>
-		<p> This website is primariy designed for potential employers and tech professionals. Ideally visitors will view this page after meeting me in person at a tech networking event.
+		<p> This website is primariy designed for potential employers and tech professionals. Ideally visitors will view
+			this page after meeting me in person at a tech networking event.
 			They may also come across my site via various social media channels.</p>
 
 		<h2>Goal:</h2>
 
 		<p>To obtain full-time <strong>employment</strong> or a Jr. developer <em>internship.</em>
 
-		<br/>
-		<br/>
+			<br/>
+			<br/>
 
 
 		<h2>Persona:</h2>
@@ -32,11 +34,14 @@
 		<p><strong>Name:</strong> Will Ventures </p>
 		<p><strong>Age:</strong> 35</p>
 		<p><strong>Occupation:</strong> Tech Recruiter </p>
-		<p><strong>Technology Used:</strong> Apple Evangelical. Will utilizes a Macbook Pro from 2016 and the newest, shiniest iPhone X.</p>
-		<p><strong>Description:</strong> Will utilizes LinkedIn and networking events to scout local tech-talent and feed them to local tech
+		<p><strong>Technology Used:</strong> Apple Evangelical. Will utilizes a Macbook Pro from 2016 and the newest,
+			shiniest iPhone X.</p>
+		<p><strong>Description:</strong> Will utilizes LinkedIn and networking events to scout local tech-talent and feed
+			them to local tech
 			companies.</p>
 		<p><strong>Goals:</strong> Find the most qualified candidates as quickly as possible.</p>
-		<p><strong>Frustration and Needs:</strong> Will gets tired of looking rummaging through the same old generic LinkedIn profiles and Resume
+		<p><strong>Frustration and Needs:</strong> Will gets tired of looking rummaging through the same old generic
+			LinkedIn profiles and Resume
 			websites.
 			Will wants content that jumps off the page and separates his desired candidates from the rest of the herd </p>
 		<p>User Story:</p>
