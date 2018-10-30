3c7b854b4e8c9d52cd41ee258e53fa3d68a2810f
dsanderson90 <dsanderson3@cnm.edu>
2018-10-30 08:53:30 -0600

    added proper directory structure

---
+++ documentation/milestone-2.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<link rel="stylesheet" href="../style.css"/>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+		<title>Personal Web Project Main Page</title>
+	</head>
+
+	<div class="header">
+
+
+		<h1><strong>Content Strategy</strong></h1>
+		<p>My Content Strategy for my PWP will be three-fold: 1) I will draw visitors in with a well designed layout that
+			exhibits my Front-End Dev skills. 2) I will have a Portfolio that shows off my front and backend skills and 3) I
+			will invite visitors to view my work on other outlets such as Github, CodePen, and Medium. </p>
+
+		My website will be split into 5 sections in the following order: The Splash Page, followed by an About section, Portfolio, Testimonials, and Contact Form.
+		I would like to use some sort of CSS animation on the Splash Page and
+
+		<h1>David Sanderson/Splash Page</h1>
+	</div>
