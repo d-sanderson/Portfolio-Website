df2fd19258796f11ef047745f68710bec22ae1c6
dsanderson90 <dsanderson3@cnm.edu>
2018-10-31 14:18:09 -0600

    added proper directory structure

--- documentation/milestone-2.php
+++ documentation/milestone-2.php
@@ -3,23 +3,50 @@
 	<head>
 		<link rel="stylesheet" href="style.css"/>
 		<meta charset="UTF-8">
-		<meta name="viewport"
-				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-		<title>Personal Web Project Main Page</title>
+		<title>PWP Milestone 2A</title>
 	</head>
 
-	<div class="header">
+	<h2><strong>Content Strategy</strong></h2>
+	<p>My Personal Website will have a simple, elegant design. I will use bootstrap to make my content mobile-friendly and
+		css styles to build out my own unique theme. My Content Strategy will be three-fold: 1) It will draw visitors
+		in with a simple layout that exhibits my Front-End Dev skills. 2) It will have a Portfolio section that shows off
+		my front and backend skills and 3) It will have a sticky footer that allows viewers to visit my work on other
+		outlets.
 
+		My website will consist of the following 6 sections:
 
-		<h1><strong>Content Strategy</strong></h1>
-		<p>My Content Strategy for my PWP will be three-fold: 1) I will draw visitors in with a well designed layout that
-			exhibits my Front-End Dev skills. 2) I will have a Portfolio that shows off my front and backend skills and 3) I
-			will invite visitors to view my work on other outlets such as Github, CodePen, and Medium. </p>
+	<h3>Splash:</h3>
+	<p>This will be the first section that someone sees when they land on my site so its important that it makes a good
+		impression! I want it to be simple but thoughtfully formatted. It will have my name, title of "Full-Stack Web
+		Developer" and perhaps a simple CSS drawing (that may change seasonally) and css gradient for a background.</p>
+	<h3>About:</h3>
+	<p>This section will include a brief "elevator pitch" about myself. I want it to highlight my skills and recent work experience (and languages I am most proficient in) while giving the reader an idea of my
+		background, personality and interests.
+	</p>
+	<h3>Portfolio:</h3>
+	<p>This section will be frequently updated as I continue to add new projects to my portfolio. I want it to have a
+		card layout with an image and description that viewers can scroll through. Th so potential employers can become more
+		familiar with my project experience and abilities. </p>
+	<h3>Testimonials:</h3>
+	<p>This portion of the site will consist of quotes from fellow deep divers that highlight their experience working
+		with me and the skillset I brought to the table. I want this section to be a carousel that cycles through various
+		testimonials.</p>
+	<h3>Contact:</h3>
+	<p> This section will be a fully functioning form that allows site visitors to reach me via email. My contact form
+		will adhere to all the guidelines put forth by Deep Dive Instructors so that it is secure and sanitizes all user
+		input.</p>
+	<h3>Footer:</h3>
+	<p>This will be the last piece and most simple part to code. It will be a sticky footer with a black background and
+		links to my other channels such as
+		Github, Twitter, Codepen, and LinkedIn.</p>
 
-		My website will be split into 5 sections in the following order: The Splash Page, followed by an About section, Portfolio, Testimonials, and Contact Form.
-		I would like to use some sort of CSS animation on the Splash Page and
+	<h2>WireFrames</h2>
 
-		<h1>David Sanderson/Splash Page</h1>
-	</div>
+	<h3>Mobile</h3>
+	<img src="Mobile%20View.png" alt="mobile mock-up">
+
+	<h3>Desktop</h3>
+	<img src="Desktop.png" alt="desktop mock-up">
+
+</html>
