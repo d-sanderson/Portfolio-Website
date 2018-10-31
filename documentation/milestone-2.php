1753917b6772765edd4e157a1789b7d678adbec3
dsanderson90 <dsanderson3@cnm.edu>
2018-10-31 14:22:38 -0600

    added proper directory structure

--- documentation/milestone-2.php
+++ documentation/milestone-2.php
@@ -13,39 +13,39 @@
 		in with a simple layout that exhibits my Front-End Dev skills. 2) It will have a Portfolio section that shows off
 		my front and backend skills and 3) It will have a sticky footer that allows viewers to visit my work on other
 		outlets.
-
+		<br/>
 		My website will consist of the following 6 sections:
 
-	<h3>Splash:</h3>
+	<h2>Splash:</h2>
 	<p>This will be the first section that someone sees when they land on my site so its important that it makes a good
 		impression! I want it to be simple but thoughtfully formatted. It will have my name, title of "Full-Stack Web
 		Developer" and perhaps a simple CSS drawing (that may change seasonally) and css gradient for a background.</p>
-	<h3>About:</h3>
+	<h2>About:</h2>
 	<p>This section will include a brief "elevator pitch" about myself. I want it to highlight my skills and recent work experience (and languages I am most proficient in) while also giving the reader an idea of my
 		background, personality and interests.
 	</p>
-	<h3>Portfolio:</h3>
+	<h2>Portfolio:</h2>
 	<p>This section will be frequently updated as I continue to add new projects to my portfolio. I want it to have a
 		card layout with an image and description that viewers can scroll through. Th so potential employers can become more
 		familiar with my project experience and abilities. </p>
-	<h3>Testimonials:</h3>
+	<h2>Testimonials:</h2>
 	<p>This portion of the site will consist of quotes from fellow deep divers that highlight their experience working
 		with me and the skillset I brought to the table. I want this section to be a carousel that cycles through various
 		testimonials.</p>
-	<h3>Contact:</h3>
+	<h2>Contact:</h2>
 	<p> This section will be a fully functioning form that allows site visitors to reach me via email. My contact form
 		will adhere to all the guidelines put forth by Deep Dive Instructors so that it is secure and sanitizes all user
 		input.</p>
-	<h3>Footer:</h3>
+	<h2>Footer:</h2>
 	<p>This will be the last piece and most simple part to code. It will be a sticky footer with a black background that includes
 		links to my other channels such as Github, Twitter, Codepen, and LinkedIn.</p>
 
 	<h2>WireFrames</h2>
 
-	<h3>Mobile</h3>
+	<h2>Mobile</h2>
 	<img src="Mobile%20View.png" alt="mobile mock-up">
 
-	<h3>Desktop</h3>
+	<h2>Desktop</h2>
 	<img src="Desktop.png" alt="desktop mock-up">
 
 </html>
